875469c499119160daa0ebb22e90a6c70c365a58
FMahdi09 <117906091+FMahdi09@users.noreply.github.com>
2026-02-23 19:27:25 +0100

    feat: users can now register and are persisted (#39)

--- Identity/Modules/Users/Users.Infrastructure/IdentityUserRepository.cs
+++ Identity/Modules/Users/Users.Infrastructure/IdentityUserRepository.cs
@@ -1,32 +1,47 @@
+using Microsoft.AspNetCore.Identity;
 using OneOf;
 using Users.Application;
 using Users.Contracts.Errors;
 using Users.Domain.Entities;
+using Users.Infrastructure.Entities;
 
 namespace Users.Infrastructure;
 
 public class IdentityUserRepository : UserRepository
 {
-    private readonly List<User> users = [];
+    private readonly UserManager<UserEntity> userManager;
 
-    public Task<User?> GetByUsername(string username, CancellationToken cancellationToken)
+    public IdentityUserRepository(UserManager<UserEntity> userManager)
     {
-        return Task.FromResult(users.FirstOrDefault(u => u.Username == username));
+        this.userManager = userManager;
     }
 
-    public Task<OneOf<User, DuplicateUsernameError>> CreateUser(
+    public async Task<User?> GetByUsername(string username, CancellationToken cancellationToken)
+    {
+        var user = await userManager.FindByNameAsync(username);
+
+        return user is null ? null : new User(user.Id, user.UserName ?? "");
+    }
+
+    public async Task<OneOf<User, DuplicateUsernameError>> CreateUser(
         User user,
         string password,
         CancellationToken cancellationToken
     )
     {
-        if (users.Any(u => u.Username == user.Username))
+        var createdUser = new UserEntity
         {
-            return Task.FromResult<OneOf<User, DuplicateUsernameError>>(new DuplicateUsernameError(user.Username));
+            Id = user.Id,
+            UserName = user.Username
+        };
+
+        var creationResult = await userManager.CreateAsync(createdUser, password);
+
+        if (!creationResult.Succeeded)
+        {
+            return new DuplicateUsernameError(user.Username);
         }
 
-        users.Add(user);
-
-        return Task.FromResult<OneOf<User, DuplicateUsernameError>>(user);
+        return new User(createdUser.Id, createdUser.UserName);
     }
 }
