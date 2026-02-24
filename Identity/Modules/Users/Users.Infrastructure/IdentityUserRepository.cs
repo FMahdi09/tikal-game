d8421c18e477884809514e4d65c0310512a5c193
Fabian Mahdi <Fabian@Mahdi.at>
2026-02-24 20:31:39 +0100

    added missing solution flag

--- Identity/Modules/Users/Users.Infrastructure/IdentityUserRepository.cs
+++ Identity/Modules/Users/Users.Infrastructure/IdentityUserRepository.cs
@@ -31,7 +31,8 @@ public class IdentityUserRepository : UserRepository
     {
         var createdUser = new UserEntity
         {
-            Id = user.Id, UserName = user.Username
+            Id = user.Id,
+            UserName = user.Username
         };
 
         var creationResult = await userManager.CreateAsync(createdUser, password);
